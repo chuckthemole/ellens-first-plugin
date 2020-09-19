0020e5a86b617a842456ee227b1f318ec71c373a
Charles Thomas <chuckthemole@gmail.com>
2020-09-19 14:11:35 -0700

    Plugin for walk speed

--- src/main/java/edu/mills/cs180a/charlesfirstplugin/CharlesFirstPlugin.java
+++ src/main/java/edu/mills/cs180a/charlesfirstplugin/CharlesFirstPlugin.java
@@ -1,5 +1,8 @@
 package edu.mills.cs180a.charlesfirstplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CharlesFirstPlugin extends JavaPlugin {
@@ -12,4 +15,30 @@ public class CharlesFirstPlugin extends JavaPlugin {
   public void onDisable() {
     getLogger().info("CharlesFirstPlugin.onDisable has been invoked!");
   }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (cmd.getName().equalsIgnoreCase("walkfast")) {
+      System.out.println("Command called for walkfast...");
+      Player p = (Player) sender;
+      p.setWalkSpeed(1);
+      return true;
+    }
+    if (cmd.getName().equalsIgnoreCase("walkslow")) {
+      System.out.println("Command called for walkfast...");
+      Player p = (Player) sender;
+      p.setWalkSpeed(1);
+      return true;
+    }
+    if (cmd.getName().equalsIgnoreCase("walknormal")) {
+      System.out.println("Command called for walkfast...");
+      Player p = (Player) sender;
+      p.setWalkSpeed(1);
+      return true;
+    }
+
+    return false;
+  }
+
+  public static void spin(Player player) {}
 }
